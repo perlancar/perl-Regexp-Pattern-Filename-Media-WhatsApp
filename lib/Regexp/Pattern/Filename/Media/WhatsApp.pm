e8d34a918ca93d8a95801ba751133326bb2d0319
perlancar (on pc-home) <perlancar@gmail.com>
2022-05-09 01:25:51 +0700

    Recognize jpeg in addition to jpg extension

--- lib/Regexp/Pattern/Filename/Media/WhatsApp.pm
+++ lib/Regexp/Pattern/Filename/Media/WhatsApp.pm
@@ -15,7 +15,7 @@ our %RE;
 $RE{filename_video_whatsapp} = {
     summary => 'Media (video, image) filename saved by WhatsApp',
     pat => qr/\A(?:
-                  (?:IMG-[0-9]{8}-WA[0-9]{4,}\.(?:JPG|jpg))|
+                  (?:IMG-[0-9]{8}-WA[0-9]{4,}\.(?:JPE?G|jpe?g))|
                   (?:VID-[0-9]{8}-WA[0-9]{4,}\.(?:MP4|mp4))
               )\z/x,
     tags => ['anchored'],
@@ -23,6 +23,7 @@ $RE{filename_video_whatsapp} = {
         {str=>'foo.jpg', matches=>0, summary=>'No pattern'},
         {str=>'foo.mp4', matches=>0, summary=>'No pattern'},
         {str=>'IMG-20210922-WA0001.jpg', matches=>1},
+        {str=>'IMG-20210922-WA0001.jpeg', matches=>1},
         {str=>'VID-20210922-WA0001.mp4', matches=>1},
         {str=>'IMG-20210922-WA0001.mp4', matches=>0, summary=>'Wrong extension'},
         {str=>'VID-20210922-WA0001.jpg', matches=>0, summary=>'Wrong extension'},
